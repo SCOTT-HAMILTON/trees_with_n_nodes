4961c1bb8299fa150deb7f5acdef47c2a54e3b9d
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2021-10-14 18:49:51 +0200

    Add shell.nix and CLI

--- src/main.rs
+++ src/main.rs
@@ -5,55 +5,41 @@ mod tree_utils;
 mod all_combinations;
 use trees_with_n_nodes::TreesWithNNodes;
 use tree_utils::print_tree;
+use argparse::{ArgumentParser,StoreTrue,Store};
 
 fn main() {
-    // let child_ps: Vec<Vec<Tree<String>>> = vec![
-    //     vec![
-    //         make_tree(1)
-    //     ],
-    //     vec![
-    //         make_tree(3),
-    //         trs("R")
-    //             /trs("1")
-    //             /(trs("2")/trs("1")),
-    //         trs("R")
-    //             /(trs("R")/trs("1")/trs("2")),
-    //         trs("R")
-    //             /(trs("R")
-    //                 /(trs("1")/trs("1"))),
-            
-    //     ]
-    // ];
-    // let order_index = [ 0, 3 ];
-    // let order: Vec<Tree<String>> = (0..order_index.len()).zip(
-    //     order_index.iter()
-    // ).map(|(index,order_index)|
-    //       child_ps[index as usize][*order_index as usize].clone())
-    // .collect();
-    // for (index,c) in child_ps.iter().enumerate() {
-    //     println!("Possibilities for child#{}", index+1);
-    //     for p in c {
-    //         println!("P:\n{}", tree_str(p.clone()));
-    //     }
-    // }
-    // let base_tree = make_tree(2);
-    // println!("Base tree:");
-    // print_tree(&base_tree);
-
-    // let repartition: Vec<usize> = vec![1, 2, 2];
-    // for p in ApplyRepartition::new(repartition) {
-    //     println!("p:");
-    //     print_tree(&p);
-    // }
-    //
-    let mut counter = 0;
-    for p in TreesWithNNodes::new(4) {
-        println!("p:");
-        print_tree(&p);
-        counter += 1;
-        if counter % 100 == 0 {
-            println!("{}", counter);
-        }
+    let mut print_trees = false;
+    let mut n_arg = "".to_string();
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Generate all possible trees with n nodes.");
+        ap.refer(&mut print_trees)
+            .add_option(&["-p", "--print"], StoreTrue,
+            "Print all possible trees");
+        ap.refer(&mut n_arg)
+            .add_argument("N", Store,
+                          "number of nodes");
+        ap.parse_args_or_exit();
     }
-    println!("{}", counter);
+
+    let n: usize;
+    match n_arg.parse::<usize>() {
+        Err(why) => {
+            println!("Invalid argument for N: `{}`, {}", n_arg, why);
+            return;
+        },
+        Ok(ok_n) => {
+            n = ok_n;
+        }
+    };
+
+    let mut counter = 0;
+    for p in TreesWithNNodes::new(n) {
+        if print_trees {
+            println!("p:");
+            print_tree(&p);
+        }
+        counter += 1;
+    }
+    println!("There are {} possible trees with {} nodes.", counter, n);
 }
